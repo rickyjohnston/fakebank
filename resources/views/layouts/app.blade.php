7bc54e66f197744f664a52d2c39ea44cf7131a4b
Ricky Johnston <richard.johnston@growthengineering.co.uk>
2018-04-02 18:33:44 +0100

    Add Links to Oauth via Menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,6 +53,12 @@
                                 </a>
 
                                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('home')}}">
+                                        {{ __('Transactions') }}
+                                    </a>
+                                    <a class="dropdown-item" href="{{ route('tokens') }}">
+                                        {{ __('API Tokens') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', 'WelcomeController@index')->name('welcome');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/tokens', 'TokenController@index')->name('token.index');
+Route::get('/tokens', 'TokenController@index')->name('tokens');
