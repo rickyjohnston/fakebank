abdd08b553f08d5c4b03fcd8476f9f013d7ce590
Ricky Johnston <richard.johnston@growthengineering.co.uk>
2018-04-02 11:19:48 +0100

    Install a datepicker

--- resources/assets/js/components/TransactionList.vue
+++ resources/assets/js/components/TransactionList.vue
@@ -1,63 +1,60 @@
 <template>
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col">
+            <div class="col-9" v-if="transactions.length > 0">
                 <h2>Transaction List</h2>
-
-                <div class="">
-                    <div class="form-group">
-                        <label for="customerId">
-                            Customer ID
-                        </label>
-                        <select class="form-control" name="customerId" id="customerId" v-model="customerId">
-                            <option v-for="customer in customers" :key="customer">
-                                {{ customer }}
-                            </option>
-                        </select>
-                    </div>
-
-                    <div class="form-group">
-                        <label for="amount">
-                            Amount
-                        </label>
-                        <input class="form-control" id="amount" name="amount" type="text" v-model="amount">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="date">
-                            Date
-                        </label>
-                        <input class="form-control" type="text" id="date" name="date" v-model="date">
-                    </div>
-
-                    <div class="form-group">
-                        <button class="btn btn-block btn-primary"
-                            @click="getFilteredSearch">
-                            Submit
-                        </button>
-                    </div>
+                <table class="table" style="width: 100%">
+                    <thead class="thead-light">
+                        <tr>
+                            <th scope="col">Customer ID</th>
+                            <th scope="col">Amount</th>
+                            <th scope="col">Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr v-for="transaction in transactions" :key="transaction.transactionId">
+                            <td v-text="transaction.customerId"></td>
+                            <td v-text="transaction.amount"></td>
+                            <td v-text="transaction.date"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-9" v-else>
+                No transactions found. 😃
+            </div>
+            <div class="col">
+                <h2>Filters</h2>
+                <div class="form-group">
+                    <label for="customerId">
+                        Customer ID
+                    </label>
+                    <select class="form-control" name="customerId" id="customerId" v-model="customerId">
+                        <option v-for="customer in customers" :key="customer">
+                            {{ customer }}
+                        </option>
+                    </select>
                 </div>
 
-                <div class="card-body" v-if="transactions.length > 0">
-                    <table class="table" style="width: 100%">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">Customer ID</th>
-                                <th scope="col">Amount</th>
-                                <th scope="col">Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="transaction in transactions" :key="transaction.transactionId">
-                                <td v-text="transaction.customerId"></td>
-                                <td v-text="transaction.amount"></td>
-                                <td v-text="transaction.date"></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="form-group">
+                    <label for="amount">
+                        Amount
+                    </label>
+                    <input class="form-control" id="amount" name="amount" type="text" v-model="amount">
                 </div>
-                <div class="card-body" v-else>
-                    No transactions found. 😃
+
+                <div class="form-group">
+                    <label for="date">
+                        Date
+                    </label>
+                    <datepicker input-class="form-control" format="yyyy-MM-dd" v-model="date" name="date" id="date"></datepicker>
+                </div>
+
+                <div class="form-group">
+                    <button class="btn btn-block btn-primary"
+                        @click="getFilteredSearch">
+                        Submit
+                    </button>
                 </div>
             </div>
         </div>
@@ -65,7 +62,11 @@
 </template>
 
 <script>
+    import Datepicker from 'vuejs-datepicker';
     export default {
+        components: {
+            Datepicker
+        },
         data() {
             return {
                 amount: '',
@@ -81,9 +82,18 @@
             this.getCustomerData();
         },
         computed: {
+            day() {
+                return this.date.getDate();
+            },
+            month() {
+                return this.date.getMonth() + 1;
+            },
+            year() {
+                return this.date.getFullYear();
+            },
             filters() {
                 let amount = (this.amount) ? { amount: this.amount } : {};
-                let date = (this.date) ? { date: this.date } : {};
+                let date = (this.date) ? { date: `${this.year}-${this.month}-${this.day}` } : {};
                 let customerId = (this.customerId) ? { customerId: this.customerId } : {};
 
                 return Object.assign({}, amount, date, customerId, { limit: 10 });
@@ -117,3 +127,9 @@
         }
     }
 </script>
+
+<style>
+    .form-control[name=date] {
+        background-color: white;
+    }
+</style>
